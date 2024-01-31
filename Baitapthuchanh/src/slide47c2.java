c78112e4c42ed8aeff95644885ec03239e056da0
MinhNgoc13 <nguyenminhngoc10h@gmail.com>
2024-01-31 20:03:10 +0700

    Baitapthuchanh

---
+++ Baitapthuchanh/src/slide47c2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class slide47c2 {
+     public static void main(String[] args) {
+        int n, sum=0;
+        Scanner sc = new Scanner(System.in);
+        while(sum<=100) {
+            System.out.println("nhap cac so: ");
+            n = sc.nextInt();
+            sum += n;
+            }
+        System.out.print("Tong = " + sum);
+    }
+}
