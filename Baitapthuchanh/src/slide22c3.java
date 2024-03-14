741470e6e86f415e944ae99d44140d897c760249
MinhNgoc13 <nguyenminhngoc10h@gmail.com>
2024-03-14 21:02:54 +0700

    Baitapthuchanh

---
+++ Baitapthuchanh/src/slide22c3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class slide22c3 {
+    public static void main(String[] args) {
+        HinhTron hinhtron = new HinhTron();
+        hinhtron.nhapBanKinh();
+        hinhtron.tinhChuVi();
+        hinhtron.tinhDienTich();
+        hinhtron.inChuVi();
+        hinhtron.inDienTich();
+    }
+}
+
+    
+}
